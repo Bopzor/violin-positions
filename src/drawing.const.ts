a196a57a28e84657867d3408951ea80680ecbd80
Bopzor <26028196+Bopzor@users.noreply.github.com>
2025-02-16 14:12:16 +0100

    split in severals components

---
+++ src/drawing.const.ts
@@ -0,0 +1,5 @@
+export const STRING_SPACING = 50;
+export const NOTE_SPACING = 80;
+export const NOTE_RADIUS = 20;
+export const FIRST_NOTE_OFFSET = NOTE_RADIUS * 3;
+export const START_STRING_OFFSET = 125;
